b4baee539a0e0a8c193baef8e5453fc1b9f1a5e4
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-18 16:49:33 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink to = '/' exact>Home Page</NavLink>
+      <NavLink to = '/movies' exact>Movies Page</NavLink>
+      <NavLink to = '/directors' exact>Directors Page</NavLink>
+      <NavLink to = '/actors' exact>Actors Page</NavLink>
     </div>
   );
 };
