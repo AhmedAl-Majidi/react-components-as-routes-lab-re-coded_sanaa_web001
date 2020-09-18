2098edb22b43a88e13abe30fe7970ef210651580
Ahmed Al-Majidi <af774857999@gmail.com>
2020-09-18 16:50:05 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,10 +4,10 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      <NavLink to = '/' exact>Home Page</NavLink>
-      <NavLink to = '/movies' exact>Movies Page</NavLink>
-      <NavLink to = '/directors' exact>Directors Page</NavLink>
-      <NavLink to = '/actors' exact>Actors Page</NavLink>
+      <NavLink to = '/' exact>Home</NavLink>
+      <NavLink to = '/movies' exact>Movies</NavLink>
+      <NavLink to = '/directors' exact>Directors</NavLink>
+      <NavLink to = '/actors' exact>Actors</NavLink>
     </div>
   );
 };
